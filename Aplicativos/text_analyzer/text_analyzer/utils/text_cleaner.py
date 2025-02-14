66f00345efeadae5721595bf3adf2f227eadabcb
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-14 07:22:18 -0300

    feat: add "stopwords"

--- Aplicativos/text_analyzer/text_analyzer/utils/text_cleaner.py
+++ Aplicativos/text_analyzer/text_analyzer/utils/text_cleaner.py
@@ -1,27 +1,30 @@
 import re
 from typing import List
+from nltk.corpus import stopwords
+import nltk
 
-def clean_text(text: str) -> List[str]:
+# Baixa as stopwords caso necessário
+nltk.download('stopwords')
+
+def clean_text(text: str, remove_stopwords: bool = True) -> List[str]:
     """
     Processa e normaliza um texto, removendo pontuação e convertendo para minúsculas.
+    Também pode remover stopwords se o parâmetro remove_stopwords for True.
 
     Args:
         text (str): O texto de entrada a ser processado.
+        remove_stopwords (bool): Se True, remove stopwords do texto.
 
     Returns:
         List[str]: Lista de palavras limpas extraídas do texto.
-    
-    Exemplo:
-        > clean_text("Olá, mundo! Isso é um teste.")
-        ['olá', 'mundo', 'isso', 'é', 'um', 'teste']
     """
-    # Converte para minúsculas
-    text = text.lower()
+    text = text.lower()  # Converte para minúsculas
+    text = re.sub(r'[^\w\s]', '', text)  # Remove pontuação
+    words = text.split()  # Divide o texto em palavras
     
-    # Remove pontuação
-    text = re.sub(r'[^\w\s]', '', text)
-    
-    # Divide em palavras
-    words = text.split()
-    
+    if remove_stopwords:
+        stop_words = set(stopwords.words("portuguese"))
+        words = [word for word in words if word not in stop_words]
+
     return words
+
