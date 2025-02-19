ddea81680727f2e3c221c2c15490307b48afd559
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-02-18 22:06:44 -0300

    feat: add metodo de similaridade jaccard

--- Aplicativos/text_analyzer/text_analyzer/comparator/text_comparator.py
+++ Aplicativos/text_analyzer/text_analyzer/comparator/text_comparator.py
@@ -26,3 +26,31 @@ class TextComparator:
         words1 = clean_text(text1)
         words2 = clean_text(text2)
         return words1, words2
+    
+    def jaccard_similarity(self, text1: str, text2: str) -> float:
+        """
+        Calcula a similaridade de Jaccard entre dois textos
+        (intersecção / união)
+        
+        Args:
+            text1: Primeiro texto
+            text2: Segundo texto
+            
+        Returns:
+            Coeficiente de similaridade de Jaccard (0-1)
+        """
+        words1, words2 = self.preprocess_texts(text1, text2)
+        
+        # Convertendo listas para conjuntos para remover duplicatas
+        set1 = set(words1)
+        set2 = set(words2)
+        
+        # Calculando intersecção e união
+        intersection = len(set1.intersection(set2))
+        union = len(set1.union(set2))
+        
+        # Evitando divisão por zero
+        if union == 0:
+            return 0
+        
+        return intersection / union
