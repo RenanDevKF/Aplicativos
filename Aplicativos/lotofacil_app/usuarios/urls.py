dd98b8f97dbcf7f44137539b456a59901b4fce39
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-11 19:27:19 -0300

    feat: add url de historico de jogos

--- Aplicativos/lotofacil_app/usuarios/urls.py
+++ Aplicativos/lotofacil_app/usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import registrar_usuario, login_usuario, logout_usuario
+from .views import registrar_usuario, login_usuario, logout_usuario, historico_jogos
 
 urlpatterns = [
     path("registro/", registrar_usuario, name="registro"),
     path("login/", login_usuario, name="login"),
     path("logout/", logout_usuario, name="logout"),
+    path("historico/", historico_jogos, name="historico"),
 ]
