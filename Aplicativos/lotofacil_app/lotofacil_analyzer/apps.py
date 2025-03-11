4216feade45ea8467825d25a5f9e135e7b227364
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-11 20:43:00 -0300

    fix: correção do nome do aplicativo

--- Aplicativos/lotofacil_app/lotofacil_analyzer/apps.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LotofacilAnalyserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lotofacil_analyser'
+    name = 'lotofacil_analyzer'
