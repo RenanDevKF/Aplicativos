774e3df93acf3c650f22fc267cd82b2a92196c8b
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-16 12:07:53 -0300

    feat:  add função para criar jogos e criar jogos rapidos

--- Aplicativos/lotofacil_app/lotofacil_analyzer/views.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'lotofacil_analyzer/home.html')
 
+from django.http import HttpResponse
+
+def criar_jogo(request):
+    return HttpResponse("Página de criação de jogo")
+
+def gerar_jogo_rapido(request):
+    return HttpResponse("Jogo rápido gerado!")
+
+
