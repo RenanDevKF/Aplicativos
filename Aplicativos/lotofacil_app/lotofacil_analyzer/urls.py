6eb2ac6f7a89b0661b9947e3574f8d9215076b02
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-16 12:11:32 -0300

    feat: add url de resultados

--- Aplicativos/lotofacil_app/lotofacil_analyzer/urls.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # Rota para a view home
     path('criar_jogo/', views.criar_jogo, name='criar_jogo'),
     path('gerar-jogo-rapido/', views.gerar_jogo_rapido, name='gerar_jogo_rapido'),
+    path('resultados/', views.resultados, name='resultados'),
 ]
 
