a82187c4806839828694daccd693038cf8d70f3c
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-16 12:07:34 -0300

    feat: criação da função de criar jogos e criar jogos rapidos

--- Aplicativos/lotofacil_app/lotofacil_analyzer/urls.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Rota para a view home
+    path('criar_jogo/', views.criar_jogo, name='criar_jogo'),
+    path('gerar-jogo-rapido/', views.gerar_jogo_rapido, name='gerar_jogo_rapido'),
 ]
+
