e16842544d239188a69aa3050d75877325d70f4f
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-18 19:59:20 -0300

    feat: add concursos e jogos gerados are admin

--- Aplicativos/lotofacil_app/lotofacil_analyzer/admin.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Concurso, JogoGerado
 
-# Register your models here.
+@admin.register(Concurso)
+class ConcursoAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'data')
+    search_fields = ('numero', 'data')
+
+@admin.register(JogoGerado)
+class JogoGeradoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'data_geracao')
+    search_fields = ('usuario__username', 'data_geracao')
