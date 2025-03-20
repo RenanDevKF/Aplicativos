5b208ad065e450b88cc5a55f813bfc115877de71
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-19 21:16:51 -0300

    refactor: refeito todo  codigo da area Concursoe mudando o nome da classe para SorteioLotofacil

--- Aplicativos/lotofacil_app/lotofacil_analyzer/models.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/models.py
@@ -1,20 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import json
 
-class Concurso(models.Model):
-    numero = models.IntegerField(unique=True)  # Número do concurso
-    data = models.DateField()
-    numeros_sorteados = models.JSONField()  # Lista de números sorteados
+class SorteioLotofacil(models.Model):
+    """Armazena resultados dos sorteios da Lotofácil."""
+    concurso = models.IntegerField(unique=True)  # Número do concurso
+    data = models.DateField()  # Data do sorteio
+    numeros = models.CharField(max_length=50)  # Números sorteados (ex: "1,2,3,4,...")
     ganhadores_15_acertos = models.IntegerField(default=0)  # Ganhadores com 15 acertos
 
+    def get_numeros_list(self):
+        """Retorna os números como lista de inteiros."""
+        return [int(n) for n in self.numeros.split(',')]
+
+    def clean(self):
+        """Valida os números sorteados."""
+        numeros = self.get_numeros_list()
+        if len(numeros) != 15:
+            raise ValidationError("O concurso deve ter exatamente 15 números.")
+        if any(n < 1 or n > 25 for n in numeros):
+            raise ValidationError("Os números devem estar entre 1 e 25.")
+
     def __str__(self):
-        return f"Concurso {self.numero} - {self.data}"
-    
-    def save(self, *args, **kwargs):
-        if len(self.numeros_sorteados) != 15:
-            raise ValidationError("O concurso deve ter exatamente 15 números sorteados.")
-        super().save(*args, **kwargs)
+        return f"Concurso {self.concurso} - {self.data}"
+
+    class Meta:
+        ordering = ['-concurso']
+        verbose_name = 'Sorteio Lotofácil'
+        verbose_name_plural = 'Sorteios Lotofácil'
 
 class JogoGerado(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
