70e7eb71b724d635b4f2b15bc982acae98f63643
Renan Kirchmaier Fayer <renan.devkf@gmail.com>
2025-03-21 20:00:02 -0300

    feat: add filtro personalizado para criar sequencia dentro do template

--- Aplicativos/lotofacil_app/lotofacil_analyzer/templatetags/custom_filters.py
+++ Aplicativos/lotofacil_app/lotofacil_analyzer/templatetags/custom_filters.py
@@ -7,3 +7,6 @@ def get_item(dictionary, key):
     """Retorna o valor de um dicionário para uma chave específica."""
     return dictionary.get(key, '')  # Retorna '' se a chave não existir
 
+@register.filter
+def range_filter(value):
+    return range(1, value + 1)
